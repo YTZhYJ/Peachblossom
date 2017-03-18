6da0c0785013eebc360ddfbdfdd1cf5adaa5275b
YTZhYJ <zhangyj_beijing@126.com>
2017-03-19 01:35:22 +0800

    测试

--- src/com/pytd/db/entity/Auth_User.java
+++ src/com/pytd/db/entity/Auth_User.java
@@ -3,9 +3,11 @@ package com.pytd.db.entity;
  * 管理人员表
  * 
  * @author zhang
- *
+ * @see http://www.open-open.com/lib/view/open1413272655903.html
  */
 public class Auth_User {
 	private Long id;
+	private String name;
+	private String password;
 	
 }
