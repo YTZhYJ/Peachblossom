56574fb6c3a1a29fc17f6941be1f0f80eaeb3754
YTZhYJ <zhangyj_beijing@126.com>
2017-03-21 01:51:33 +0800

    添加文件

--- src/com/pytd/db/dao/impl/AuthUserDaoImpl.java
+++ src/com/pytd/db/dao/impl/AuthUserDaoImpl.java
@@ -4,14 +4,5 @@ import com.pytd.db.dao.AuthUserDao;
 
 public class AuthUserDaoImpl implements AuthUserDao {
 
-	AuthUserDao authUserDao;
-
-	public AuthUserDao getAuthUserDao() {
-		return authUserDao;
-	}
-
-	public void setAuthUserDao(AuthUserDao authUserDao) {
-		this.authUserDao = authUserDao;
-	}
 	
 }
